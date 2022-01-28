930cb6bd428c6b70c1b4de3b61a3a9dc9c895e0e
Just Seal <44335975+Sealthrux@users.noreply.github.com>
2022-01-28 19:57:28 +0000

    BURGER A FUNCIONAR

--- Perene.js
+++ Perene.js
@@ -1,23 +1,23 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger')
-    const nav = document.querySelector('.topbar-ul')
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.topbar-ul');
     const navLinks = document.querySelectorAll('.topbar-hiper li');
     //toggle
 
-burger.addEventListener('click',()=>{
+    burger.addEventListener('click',()=>{
         nav.classList.toggle('navActive');
         
 //animate the hipers
         navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                 link.style.animation = ''
+            if (link.style.animation){
+                 link.style.animation = '';
             }
             else{
              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
             }
          });
+         burger.classList.toggle('toggle');
     });
-
 
 }
 
